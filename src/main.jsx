eb32d652fbcbbc91b7df09e7cd7cf386f487e987
Regiane <regianemelo1995@gmail.com>
2024-08-27 11:54:07 -0300

    carregando post individual

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
 // páginas
 import Home from './routes/Home.jsx'
 import NewPost from './routes/NewPost.jsx'
+import Post from './routes/Post.jsx'
 
 import './index.css'
 
@@ -23,6 +24,10 @@ const router = createBrowserRouter([
         path: "/new",
         element: <NewPost />,
       },
+      {
+        path: "/posts/:id",
+        element: <Post />
+      }
     ],
   },
 ])
