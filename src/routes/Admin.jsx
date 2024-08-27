0549e87e970a02be0b0147cfd1ca5570734f76d2
Regiane <regianemelo1995@gmail.com>
2024-08-27 14:59:01 -0300

    deletando posts

--- src/routes/Admin.jsx
+++ src/routes/Admin.jsx
@@ -22,6 +22,14 @@ const Admin = () => {
         }
     }
 
+    const deletePost = async(id) => {
+        await blogFetch.delete(`/posts/${id}`)
+
+        const filteredPosts = posts.filter((post) => post.id !== id)
+
+        setPosts(filteredPosts)
+    }
+
     useEffect(() => {
         getPosts()
     }, [])
@@ -35,7 +43,10 @@ const Admin = () => {
                     <h2>{post.title}</h2>
                     <div className="actions">
                        <Link className='btn edit-btn'>Editar</Link>
-                       <button className='btn delete-btn'>Excluir</button>
+                       <button 
+                        className='btn delete-btn' 
+                        onClick={()=> deletePost(post.id)}>
+                            Excluir</button>
                     </div>
                 </div>
             ))
