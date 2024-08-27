90cbf590b431d52277a93c75dfe7d6eb5e2928a5
Regiane <regianemelo1995@gmail.com>
2024-08-27 10:35:04 -0300

    pagina de criacao de post

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import './NewPost.css'
 
 const NewPost = () => {
   return (
-    <div>NewPost</div>
+    <div className="new-post">
+      <h2>Inserir novo Post:</h2>
+      <form>
+        <div className="form-control">
+          <label htmlFor="title">Título</label>
+          <input 
+            type="text" 
+            name="title" 
+            id= "title" 
+            placeholder="Digite o título"
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="body">Conteúdo</label>
+          <textarea 
+            name="body" 
+            id= "body" 
+            placeholder="Digite o conteúdo"
+          />
+        </div>
+        <input type="submit" value="Criar Post" className="btn" />
+      </form>
+    </div>
   )
 }
 
